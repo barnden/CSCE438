8ff8a4b97e2a376d2f994732e08ab2feabf88318
bgn <brandon@barnden.dev>
2022-01-26 09:31:01 -0600

    Initialize repo

---
+++ crsd.c
@@ -0,0 +1,14 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "interface.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+}
