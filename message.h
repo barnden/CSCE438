b442c4e964bac746a2d66deba9b60b5f56da6219
barnden <brandon@nguyen.vc>
2022-01-26 16:46:56 -0600

    Add message.h

---
+++ message.h
@@ -0,0 +1,10 @@
+#pragma once
+
+enum MessageType { CREATE,   // Create new room              (client  -> server)
+                   DELETE,   // Delete room                  (client  -> server)
+                   JOIN,     // Join a room                  (client  -> server)
+                   LIST,     // List all rooms               (client  -> server)
+                   MESSAGE,  // A chat message               (client <-> server)
+                   RESPONSE, // Response from other commands (server  -> client)
+                   INVALID
+                   };
