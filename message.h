edf320528aa4f7975bff323dc8b807d16deff16d
barnden <brandon@nguyen.vc>
2022-01-26 21:54:32 -0600

    Remove unused MESSAGE type

--- message.h
+++ message.h
@@ -4,7 +4,6 @@ enum MessageType { CREATE,   // Create new room              (client  -> server)
                    DELETE,   // Delete room                  (client  -> server)
                    JOIN,     // Join a room                  (client  -> server)
                    LIST,     // List all rooms               (client  -> server)
-                   MESSAGE,  // A chat message               (client <-> server)
                    RESPONSE, // Response from other commands (server  -> client)
                    INVALID
                    };
