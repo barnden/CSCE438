0880968d03240d8ddfa0ddf8d4c547f147277c94
bgn <brandon@barnden.dev>
2022-01-27 11:23:03 -0600

    Move MP1 to subdirectory

--- MP1/message.h
+++ MP1/message.h
@@ -1,7 +1,7 @@
 #pragma once
 
 enum MessageType { CREATE,   // Create new room              (client  -> server)
-                   DELETE,   // Delete room                  (client  -> server)
+                   DELETE,   // Delete room                  (client <-> server)
                    JOIN,     // Join a room                  (client  -> server)
                    LIST,     // List all rooms               (client  -> server)
                    RESPONSE, // Response from other commands (server  -> client)
