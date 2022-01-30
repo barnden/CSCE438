ac93f9945cfbbd435681a10c35bb8f9272d8d00f
bgn <brandon@barnden.dev>
2022-01-29 23:04:35 -0600

    Make MessageType::INVALID = 0

--- MP1/message.h
+++ MP1/message.h
@@ -1,9 +1,9 @@
 #pragma once
 
-enum MessageType { CREATE,   // Create new room              (client  -> server)
-                   DELETE,   // Delete room                  (client <-> server)
-                   JOIN,     // Join a room                  (client  -> server)
-                   LIST,     // List all rooms               (client  -> server)
-                   RESPONSE, // Response from other commands (server  -> client)
-                   INVALID
-                   };
+enum MessageType { INVALID,
+                   CREATE,      // Create new room              (client  -> server)
+                   DELETE,      // Delete room                  (client <-> server)
+                   JOIN,        // Join a room                  (client  -> server)
+                   LIST,        // List all rooms               (client  -> server)
+                   RESPONSE,    // Response from other commands (server  -> client)
+};
